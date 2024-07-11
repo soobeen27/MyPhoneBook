f47cb810f9175dc032ce779c57bfdfbc1b6b105b
soobeen27 <youdew27@gmail.com>
2024-07-11 11:17:20 +0900

    Initial Commit

---
+++ MyPhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhoneBook
+//
+//  Created by Soo Jang on 7/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
